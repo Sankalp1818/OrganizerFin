ffa0bc801aa39ba70041403394617cd6cee715f0
Sankalp1818 <111915307+Sankalp1818@users.noreply.github.com>
2022-09-22 12:30:28 -0500

    Initial Commit

---
+++ OrganizerFin/OrganizerFinApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizerFinApp.swift
+//  OrganizerFin
+//
+//  Created by Sankalp Srinatha on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct OrganizerFinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
